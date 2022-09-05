5ea369d07319081fbf347fb16daa44bb63ce0e8d
Rohan Chandra <rohan.chandra@developersinstitute.co.nz>
2022-09-05 13:39:54 +1200

    all styles updated for calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">1234352 *</div>
+        <div className="current-operand">14312351352638967206397236616531</div>
       </div>
+
       <button className="span-two">AC</button>
       <button>DEL</button>
       <button>÷</button>
@@ -22,8 +23,8 @@ function App() {
       <button>8</button>
       <button>9</button>
       <button>-</button>
-      <button>.</button>
       <button>0</button>
+      <button>.</button>
       <button className="span-two">=</button>
     </div>
   );
